840d7cbd5f87dff463a45e01db74a39d910d3296
dbjdbj <dbj@dbj.org>
2018-01-18 18:24:03 +0100

    comparing fro speed fast-deep-equal and dbj comparators mixed with fast-deep-equal as secondary comparator

--- dbj_cond_test/toolbench/benchmark/index.js
+++ dbj_cond_test/toolbench/benchmark/index.js
@@ -1,56 +1,65 @@
 'use strict';
 
-const assertDeepStrictEqual = require('assert').deepStrictEqual;
-const tests = require('../spec/tests');
+require("dbj.cond.comparators");
+const color = require("colors");
+const fast_deep_equal = require('fast-deep-equal');
+const strictEqual = require('assert').deepStrictEqual;
+const tests = require('../dbj_comparators_tests/testlist');
 const Benchmark = require('benchmark');
 const suite = new Benchmark.Suite;
 
 
+
+
 const equalPackages = {
-  'fast-deep-equal': require('../index'),
+    'fast-deep-equal': fast_deep_equal,
+    /*
+    'shallow-equal-fuzzy': require('shallow-equal-fuzzy'),
   'nano-equal': true,
-  'shallow-equal-fuzzy': true,
   'underscore.isEqual': require('underscore').isEqual,
   'lodash.isEqual': require('lodash').isEqual,
   'deep-equal': true,
   'deep-eql': true,
-  'assert.deepStrictEqual': (a, b) => {
-    try { assertDeepStrictEqual(a, b); return true; }
-    catch(e) { return false; }
-  }
+    */
+    'dbj cond arr' : dbj.compare.arr ,
+    'dbj cond lookup' : dbj.compare.lookup 
 };
 
+console.log("\nBenchmarking\n".underline.padStart(60));
 
 for (const equalName in equalPackages) {
   let equalFunc = equalPackages[equalName];
   if (equalFunc === true) equalFunc = require(equalName);
-
+    /*
   for (const testSuite of tests) {
-    for (const test of testSuite.tests) {
+      for (const test of testSuite.tests) {
       try {
-        if (equalFunc(test.value1, test.value2) !== test.equal)
-          console.error('different result', equalName, testSuite.description, test.description);
+          if (equalFunc(test.value1, test.value2, fast_deep_equal) !== test.equal)
+          console.error('different result'.red, equalName, testSuite.description, test.description);
       } catch(e) {
-        console.error(equalName, testSuite.description, test.description, e);
+        console.error("Exception ".red, equalName, testSuite.description, test.description, e.bold.red);
       }
     }
   }
-
+    */
+  console.log((equalName + "\tbenchmarking started").padStart(60));
   suite.add(equalName, function() {
     for (const testSuite of tests) {
       for (const test of testSuite.tests) {
         if (test.description != 'pseudo array and equivalent array are not equal')
-          equalFunc(test.value1, test.value2);
+            equalFunc(test.value1, test.value2, fast_deep_equal);
       }
     }
   });
 }
 
-console.log();
+console.log("\nRezults\n".underline.padStart(60));
 
 suite
   .on('cycle', (event) => console.log(String(event.target)))
   .on('complete', function () {
-    console.log('The fastest is ' + this.filter('fastest').map('name'));
+      console.log(
+          ('\nThe fastest is ' + this.filter('fastest').map('name') + "\n").bold.cyan
+      );
   })
   .run({async: true});
