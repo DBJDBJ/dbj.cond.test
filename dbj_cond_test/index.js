2bf1d4f11ba2fd71610697d755710da6cba3a9b8
dbjdbj <dbj@dbj.org>
2018-01-19 11:15:21 +0100

    all tests are go

--- dbj_cond_test/index.js
+++ dbj_cond_test/index.js
@@ -12,8 +12,8 @@ See the License for the specific language governing permissions and limitations 
 */
 'use strict';
 
-// require("./toolbench/dbj/dbj.cond.simplex").run();
-// require("./toolbench/dbj/dbj.cond.complex").run();
+require("./toolbench/dbj/dbj.cond.simplex").run();
+require("./toolbench/dbj/dbj.cond.complex").run();
 require("./toolbench/dbj_comparators_tests/testrun").run();
-// require("./toolbench/benchmark/index").run();
+require("./toolbench/benchmark/index").run();
 // eof
